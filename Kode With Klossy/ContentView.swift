1c8d190b9ef89f6ea9d534ea79c6514fbf30413c
03priyak <56941199+03priyak@users.noreply.github.com>
2020-07-08 13:28:49 -0400

    Initial Commit

---
+++ Kode With Klossy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kode With Klossy
+//
+//  Created by Priya on 7/8/20.
+//  Copyright © 2020 Priya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kode With KlossyTests/Kode_With_KlossyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kode_With_KlossyTests.swift
+//  Kode With KlossyTests
+//
+//  Created by Priya on 7/8/20.
+//  Copyright © 2020 Priya. All rights reserved.
+//
+
+import XCTest
+@testable import Kode_With_Klossy
+
+class Kode_With_KlossyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
